9397e4d9942c557d8f59f390ac2f320eee418c37
pavelkalinchuk <715m4s@gmail.com>
2024-12-07 23:14:12 +0300

    Реализован тест по csv-файлу

--- test_homework.py
+++ test_homework.py
@@ -1,13 +1,14 @@
+import csv
+import io
 import os
 from zipfile import ZipFile
-
 
 current_directory_path = os.path.dirname(__file__)
 resources_directory_path = os.path.join(current_directory_path, "resources")
 tmp_directory_path = os.path.join(current_directory_path, "tmp")
 archive_file = os.path.join(resources_directory_path, "archive.zip")
 
-with ZipFile (archive_file, 'w') as zfile:
+with ZipFile(archive_file, 'w') as zfile:
     # Проходим по всем файлам в директории
     for root, dirs, files in os.walk(tmp_directory_path):
         for file in files:
@@ -15,3 +16,20 @@ with ZipFile (archive_file, 'w') as zfile:
             file_path = os.path.join(root, file)
             # # Добавляем файл в архив
             zfile.write(file_path, os.path.relpath(file_path, resources_directory_path))
+    print(zfile.namelist())
+
+
+# Читаем csv-файл расположенный в архиве
+def test_csv():
+    with ZipFile(archive_file, 'r') as zfile:
+        # Открываем CSV-файл внутри архива
+        with zfile.open("../tmp/test.csv") as csv_file:
+            # Создаем объект CSV-ридера
+            csv_reader = csv.reader(io.TextIOWrapper(csv_file, encoding='utf-8'))
+            # Читаем строки из CSV-файла и записываем в список
+            csv_content = []
+            for row in csv_reader:
+                csv_content.append(row)
+            assert csv_content, "Файл пустой!"
+            assert len(csv_content) == 3, "Количество строк не соответствует ожидаемому"
+            assert "Фамилия; Имя; Отчество; Пол; Возраст" in csv_content[0]
