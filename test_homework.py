8bbd8359d7fc62e70b117fdc527b4d05e3fdb6f6
pavelkalinchuk <715m4s@gmail.com>
2024-12-08 15:51:30 +0300

    Реализован тест по csv-файлу

--- test_homework.py
+++ test_homework.py
@@ -2,12 +2,14 @@ import csv
 import io
 import os
 from zipfile import ZipFile
+from openpyxl import load_workbook
 
 current_directory_path = os.path.dirname(__file__)
 resources_directory_path = os.path.join(current_directory_path, "resources")
 tmp_directory_path = os.path.join(current_directory_path, "tmp")
 archive_file = os.path.join(resources_directory_path, "archive.zip")
 
+# Создаём архив 'archive.zip' с файлами из каталога 'tmp' и помещаем в каталог 'resources'4
 with ZipFile(archive_file, 'w') as zfile:
     # Проходим по всем файлам в директории
     for root, dirs, files in os.walk(tmp_directory_path):
@@ -19,11 +21,11 @@ with ZipFile(archive_file, 'w') as zfile:
     print(zfile.namelist())
 
 
-# Читаем csv-файл расположенный в архиве
+# Читаем csv-файл расположенный в архиве и выполняем проверки содержимого
 def test_csv():
-    with ZipFile(archive_file, 'r') as zfile:
+    with ZipFile(archive_file, 'r') as zfile_csv:
         # Открываем CSV-файл внутри архива
-        with zfile.open("../tmp/test.csv") as csv_file:
+        with zfile_csv.open("../tmp/test.csv") as csv_file:
             # Создаем объект CSV-ридера
             csv_reader = csv.reader(io.TextIOWrapper(csv_file, encoding='utf-8'))
             # Читаем строки из CSV-файла и записываем в список
@@ -33,3 +35,15 @@ def test_csv():
             assert csv_content, "Файл пустой!"
             assert len(csv_content) == 3, "Количество строк не соответствует ожидаемому"
             assert "Фамилия; Имя; Отчество; Пол; Возраст" in csv_content[0]
+
+
+# Читаем xlsx-файл расположенный в архиве и выполняем проверки содержимого
+def test_xlsx():
+    with ZipFile(archive_file, 'r') as zfile_xlsx:
+        # Открываем xlsx-файл внутри архива
+        with zfile_xlsx.open("../tmp/book.xlsx") as xlsx_file:
+            file_content = io.BytesIO(xlsx_file.read())
+            workbook = load_workbook(file_content)
+            sheet = workbook.active
+            assert sheet['A1'].value == "Ячейка А1"
+            assert sheet['C10'].value == "Ячейка С10"
